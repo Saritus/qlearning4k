3e3b1a1dce05bf9c87ecf0915a3a85344a6896fa
Sebastian Mischke <SebastianMischke@gmx.de>
2017-08-03 16:17:33 +0200

    Use nb_actions for creation of model

--- qlearn.py
+++ qlearn.py
@@ -8,14 +8,15 @@ nb_frames = 1
 grid_size = 10
 hidden_size = 100
 
+game = Snake(grid_size)
+
 model = Sequential()
 model.add(Flatten(input_shape=(nb_frames, grid_size, grid_size)))
 model.add(Dense(hidden_size, activation='relu'))
 model.add(Dense(hidden_size, activation='relu'))
-model.add(Dense(3))
+model.add(Dense(game.nb_actions, activation='softmax'))
 model.compile(sgd(lr=.2), "mse")
 
-game = Snake(grid_size)
 agent = Agent(model)
 agent.train(game)
 agent.play(game)
