3aca69467d8778c27703c8d8771965de44dee542
Sebastian Mischke <SebastianMischke@gmx.de>
2017-08-03 18:10:28 +0200

    Fix array assignments

--- qlearning4k/games/Obstacles.py
+++ qlearning4k/games/Obstacles.py
@@ -2,6 +2,7 @@ __author__ = "Sebastian Mischke"
 
 import numpy as np
 from .game import Game
+import copy
 
 
 class Obstacles(Game):
@@ -12,7 +13,7 @@ class Obstacles(Game):
 
     def reset(self):
         self.board = np.zeros((self.grid_size, self.grid_size))
-        self.player = (int(self.grid_size / 2), self.grid_size - 1)
+        self.player = [self.grid_size - 1, int(self.grid_size / 2)]
 
     @property
     def name(self):
@@ -23,41 +24,44 @@ class Obstacles(Game):
         return 5
 
     def play(self, action):
-        # Left
-        if (action is 0
-            and self.player[0] > 0
-            and self.board[self.player[0] - 1, self.player[1]] is 0):
-            self.player[0] -= 1
-        # Right
-        elif (action is 1
-              and self.player[0] < self.grid_size - 1
-              and self.board[self.player[0] + 1, self.player[1]] is 0):
-            self.player[0] += 1
         # Up
-        elif (action is 2
-              and self.player[1] > 0
-              and self.board[self.player[0], self.player[1] - 1] is 0):
-            self.player[1] -= 1
+        if (action == 0
+            and self.player[0] > 0
+            and self.board[self.player[0] - 1, self.player[1]] == 0):
+            self.player[0] -= 1
         # Down
-        elif (action is 3
-              and self.player[1] > self.grid_size - 1
-              and self.board[self.player[0], self.player[1] + 1] is 0):
+        elif (action == 1
+              and self.player[0] < self.grid_size - 1
+              and self.board[self.player[0] + 1, self.player[1]] == 0):
+            self.player[0] += 1
+        # Left
+        elif (action == 2
+              and self.player[1] > 0
+              and self.board[self.player[0], self.player[1] - 1] == 0):
+            self.player[1] -= 1
+        # Right
+        elif (action == 3
+              and self.player[1] < self.grid_size - 1
+              and self.board[self.player[0], self.player[1] + 1] == 0):
             self.player[1] += 1
+
+        self.board = np.roll(self.board, 1, axis=0)
+        self.board[0] = np.zeros(self.grid_size)
+        self.board[0][np.random.randint(0, self.grid_size)] = 1
 
     def get_state(self):
         canvas = copy.deepcopy(self.board)
-        canvas[self.player] = 2
+        canvas[self.player[0], self.player[1]] = 2
         return np.asarray(canvas)
 
     def get_score(self):
-        if self.board[self.player] is 1:
+        if self.board[self.player[0], self.player[1]] == 1:
             return -1
         else:
             return 0
 
     def is_over(self):
-        return self.board[self.player] is 1
+        return self.board[self.player[0], self.player[1]] == 1
 
     def is_won(self):
-        fruit_row, fruit_col, basket = self.state[0]
-        return fruit_row == self.grid_size - 1 and abs(fruit_col - basket) <= 1
+        return False
