9dbb7732eb5eeaa3e80244a39858faa9ce87ffe9
Sebastian Mischke <SebastianMischke@gmx.de>
2017-08-05 06:51:03 +0200

    Add frogger with obstacles code

--- qlearning4k/games/__init__.py
+++ qlearning4k/games/__init__.py
@@ -1,3 +1,4 @@
 from .catch import Catch
 from .snake import Snake
 from .obstacles import Obstacles
+from .frogger import Frogger
---
+++ qlearning4k/games/frogger.py
@@ -0,0 +1,69 @@
+__author__ = "Sebastian Mischke"
+
+import numpy as np
+from .game import Game
+import copy
+
+
+class Frogger(Game):
+    def __init__(self, grid_size=10, obstacles=1):
+        self.grid_size = grid_size
+        self.obstacles = obstacles
+        self.won = False
+        self.reset()
+
+    def reset(self):
+        self.board = np.zeros((self.grid_size, self.grid_size))
+        self.player = [self.grid_size - 1, int(self.grid_size / 2)]
+
+    @property
+    def name(self):
+        return "Obstacles"
+
+    @property
+    def nb_actions(self):
+        return 5
+
+    def play(self, action):
+        # Up
+        if (action == 0
+            and self.player[0] > 0
+            and self.board[self.player[0] - 1, self.player[1]] == 0):
+            self.player[0] -= 1
+        # Down
+        elif (action == 1
+              and self.player[0] < self.grid_size - 1
+              and self.board[self.player[0] + 1, self.player[1]] == 0):
+            self.player[0] += 1
+        # Left
+        elif (action == 2
+              and self.player[1] > 0
+              and self.board[self.player[0], self.player[1] - 1] == 0):
+            self.player[1] -= 1
+        # Right
+        elif (action == 3
+              and self.player[1] < self.grid_size - 1
+              and self.board[self.player[0], self.player[1] + 1] == 0):
+            self.player[1] += 1
+
+        self.board = np.roll(self.board, 1, axis=0)
+        self.board[0] = np.zeros(self.grid_size)
+        for _ in range(self.obstacles):
+            self.board[0][np.random.randint(0, self.grid_size)] = 1
+
+    def get_state(self):
+        canvas = copy.deepcopy(self.board)
+        canvas[self.player[0], self.player[1]] = 2
+        return np.asarray(canvas)
+
+    def get_score(self):
+        if self.board[self.player[0], self.player[1]] == 1:
+            return -1
+        else:
+            return self.grid_size - self.player[0] - 1
+
+    def is_over(self):
+        return self.board[self.player[0], self.player[1]] == 1 or self.player[0] == 0
+
+    def is_won(self):
+        return self.player[0] == 0
