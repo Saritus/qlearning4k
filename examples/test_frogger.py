04c4e030fb33bc46f798ab323df845e838104079
Sebastian Mischke <SebastianMischke@gmx.de>
2017-08-05 06:57:17 +0200

    Use rows and cols

--- examples/test_frogger.py
+++ examples/test_frogger.py
@@ -4,18 +4,19 @@ from qlearning4k.games import Frogger
 from keras.optimizers import *
 from qlearning4k import Agent
 
-grid_size = 10
+rows = 10
+cols = 10
 hidden_size = 100
 nb_frames = 1
 
 model = Sequential()
-model.add(Flatten(input_shape=(nb_frames, grid_size, grid_size)))
+model.add(Flatten(input_shape=(nb_frames, rows, cols)))
 model.add(Dense(hidden_size, activation='relu'))
 model.add(Dense(hidden_size, activation='relu'))
 model.add(Dense(5, activation='softmax'))
 model.compile(sgd(lr=.2), "mse")
 
-game = Frogger(grid_size)
+game = Frogger(rows, cols)
 agent = Agent(model=model)
 agent.train(game, batch_size=50, nb_epoch=10000, epsilon_rate=0.2)
 agent.play(game, nb_epoch=10)
